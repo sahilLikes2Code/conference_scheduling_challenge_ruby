ec955128fd2410038a526f5b65f7b1b5a19f9e5c
sahil kapoor <sahil.kapoor440@gmail.com>
2020-08-05 03:00:33 +0530

    group lightning talks by sorting hash

--- main.rb
+++ main.rb
@@ -8,21 +8,25 @@ track2 = Track2.new
 remaining_talks = RemainingTalks.new
 
 $All_five_min_talks = Talks::EVENTS_LIST_WITH_TIME.select { |key, hash| hash == 5 }
-puts $All_five_min_talks
+# puts $All_five_min_talks
 
-for i in Talks::EVENTS_LIST_WITH_TIME
-  # if $all_five_min_talks.include?(i[0])
-  #   puts i[0]
-  # end
-  # puts i[1].class
-  # puts i
+list_of_events_with_duration = Talks::EVENTS_LIST_WITH_TIME.sort_by { |k, v| v }
 
-  # if track1.is_lighnting_talk?(i[1])
-  # end
-
+for i in list_of_events_with_duration
+  # puts $All_five_min_talks.length * 5
+  if ($All_five_min_talks.length * 5 < track1.remaining_time_morning_slot)
+  end
   if track1.remaining_time_morning_slot >= i[1]
-    track1.add_morning_talk(i[0], i[1])
-    track1.update_morning_time(i[1])
+    if track1.is_lighnting_talk?(i[1])
+      for t in $All_five_min_talks
+        track1.add_morning_talk(t[0], t[1])
+        track1.update_morning_time(t[1])
+      end
+    elsif track1.add_morning_talk(i[0], i[1])
+      track1.update_morning_time(i[1])
+    end
+    # track1.add_morning_talk(i[0], i[1])
+    # track1.update_morning_time(i[1])
   elsif track2.remaining_time_morning_slot >= i[1]
     track2.add_morning_talk(i[0], i[1])
     track2.update_morning_time(i[1])
@@ -38,26 +42,14 @@ for i in Talks::EVENTS_LIST_WITH_TIME
 end
 
 puts "Track 1"
-puts
 puts track1.fetch_talks("morning")
 puts "12:00 PM Lunch Time"
 puts track1.fetch_talks("evening")
 puts
 puts "Track 2"
-puts
 puts track2.fetch_talks("morning")
 puts "12:00 PM Lunch Time"
 puts track2.fetch_talks("evening")
 puts
 puts "Remaining talks"
 puts "#{remaining_talks.fetch_talks.empty? ? "None" : remaining_talks.fetch_talks}"
-
-# if track1.is_lighnting_talk?(i[1])
-
-#   track1.add_morning_talk(x[0], x[1])
-#   track1.update_morning_time(x[1])
-# end
-# else
-# track1.add_morning_talk(i[0], i[1])
-# track1.update_morning_time(i[1])
-# end
