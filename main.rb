4477bf86a80d085c3f394e61f447a130deb95126
sahil kapoor <sahil.kapoor440@gmail.com>
2020-08-04 22:30:56 +0530

    add talks in 2 different tracks

--- main.rb
+++ main.rb
@@ -1,2 +1,32 @@
 require_relative "talks"
+require_relative "track1"
+require_relative "track2"
 
+track1 = Track1.new
+track2 = Track2.new
+
+for i in Talks::EVENTS_LIST_WITH_TIME
+  if track1.remaining_time_morning_slot >= i[1]
+    track1.add_morning_talk(i[0])
+    track1.update_morning_time(i[1])
+  elsif track2.remaining_time_morning_slot >= i[1]
+    track2.add_morning_talk(i[0])
+    track2.update_morning_time(i[1])
+  elsif track1.remaining_time_evening_slot >= i[1]
+    track1.add_evening_talk(i[0])
+    track1.update_evening_time(i[1])
+  elsif track2.remaining_time_evening_slot >= i[1]
+    track2.add_evening_talk(i[0])
+    track2.update_evening_time(i[1])
+  else
+  end
+end
+
+puts track1.fetch_talks("morning")
+print "\n"
+puts track1.fetch_talks("evening")
+print "\n"
+puts track2.fetch_talks("morning")
+print "\n"
+puts track2.fetch_talks("evening")
+print "\n"
--- track2.rb
+++ track2.rb
@@ -1,19 +1,41 @@
 class Track2
   def initialize
-    @talks = []
-    @morning_time = 180
-    @evening_time = 240
+    @morning_talks = []
+    @evening_talks = []
+    @morning_slot_time = 180
+    @evening_slot_time = 240
+    @begin_time = 9
   end
 
-  def add_talk(talk)
-    @talks << talk
+  def remaining_time_morning_slot
+    @morning_slot_time
   end
 
-  def morning_slot_remaining_time(talk_length_in_mins)
+  def remaining_time_evening_slot
+    @evening_slot_time
+  end
+
+  def add_morning_talk(talk)
+    @morning_talks << talk
+  end
+
+  def add_evening_talk(talk)
+    @evening_talks << talk
+  end
+
+  def fetch_talks(a)
+    a == "morning" ? @morning_talks : @evening_talks
+  end
+
+  def update_morning_time(talk_length_in_mins)
     @morning_slot_time -= talk_length_in_mins
   end
 
-  def evening_slot_remaining_time(talk_length_in_mins)
+  def update_evening_time(talk_length_in_mins)
     @evening_slot_time -= talk_length_in_mins
+  end
+
+  def is_lighnting_talk?(talk)
+    talk[1] == 5
   end
 end
