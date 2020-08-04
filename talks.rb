724eba14732d65672c69f37ad0f98266d058580d
sahil kapoor <sahil.kapoor440@gmail.com>
2020-08-04 21:50:43 +0530

    create talks class with hash of events

---
+++ talks.rb
@@ -0,0 +1,25 @@
+class Talks
+  EVENTS_LIST_WITH_TIME = {
+    "Pryin open the black box 60 minutes" => 60,
+    "Migrating a huge production codebase from sinatra to Rails 45 minutes" => 45,
+    "How does bundler work 30 minutes" => 30,
+    "Sustainable Open Source 45 minutes" => 45,
+    "How to program with Accessiblity in Mind 45 minutes" => 45,
+    "Riding Rails for 10 years lightning talk" => 5,
+    "Implementing a strong code review culture 60 minutes" => 60,
+    "Scaling Rails for Black Friday 45 minutes" => 45,
+    "Docker isn't just for deployment 30 minutes" => 30,
+    "Callbacks in Rails 30 minutes" => 30,
+    "Microservices, a bittersweet symphony 45 minutes" => 45,
+    "Teaching github for poets 60 minutes" => 60,
+    "Test Driving your Rails Infrastucture with Chef 60 minutes" => 60,
+    "SVG charts and graphics with Ruby 45 minutes" => 45,
+    "Interviewing like a unicorn: How Great Teams Hire 30 minutes" => 30,
+    "How to talk to humans: a different approach to soft skills 30 minutes" => 30,
+    "Getting a handle on Legacy Code 60 minutes" => 60,
+    "Heroku: A year in review 30 minutes" => 30,
+    "Ansible : An alternative to chef lightning talk" => 5,
+    "Ruby on Rails on Minitest 30 minutes" => 30,
+
+  }
+end
